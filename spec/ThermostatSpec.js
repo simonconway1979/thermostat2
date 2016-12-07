fcec2486ab145ebba5fd87a0268181af4c9f2573
Chris <chrixs99@hotmail.com>
2016-12-07 17:23:35 +0000

    power save functionality

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -16,15 +16,32 @@ describe("Thermostat", function() {
     expect(thermostat.temperature).toEqual(DEFAULT_TEMPERATURE+1)
   });
 
-  it("should decrease by 1°C when when lower temperature is called", function(){
+  it("Should decrease by 1°C when when lower temperature is called", function() {
     thermostat.lowerTemperature()
     expect(thermostat.temperature).toEqual(DEFAULT_TEMPERATURE-1)
   });
 
-  it("should throw error when lower temperature is called and temperature is at or below the minimum temperature", function(){
+  it("Should throw error when lower temperature is called and temperature is at or below the minimum temperature", function() {
     thermostat.temperature = 10
     expect(function(){ thermostat.lowerTemperature(); }).toThrowError("Minimum temperature reached")
   });
 
+  it("Should throw error when raise temperature is called and temperature is at or above the power save max temperature", function() {
+    thermostat.temperature = 25
+    expect(function(){ thermostat.raiseTemperature(); }).toThrowError("Maximum temperature reached")
+  });
 
+  it("Should have power save on as default", function () {
+    expect(thermostat.powerSave).toEqual(true)
+  });
+
+  it("Should have power save on as default", function () {
+    thermostat.changePowerSaveMode()
+    expect(thermostat.powerSave).toEqual(false)
+  });
+
+  // it("should throw error when raise temperature is called and temperature is at or above the maximum temperature", function(){
+  //   thermostat.temperature = 32
+  //   expect(function(){ thermostat.raiseTemperature(); }).toThrowError("Maximum temperature reached")
+  // });
 });
--- src/Thermostat.js
+++ src/Thermostat.js
@@ -1,18 +1,30 @@
 var DEFAULT_TEMPERATURE = 20
 var MINIMUM_TEMPERATURE = 10
+var POWER_SAVE_ON_MAX_TEMP = 25
+var POWER_SAVE_OFF_MAX_TEMP = 32
 
 function Thermostat() {
 this.temperature = DEFAULT_TEMPERATURE;
+this.maximumTemperature = POWER_SAVE_ON_MAX_TEMP;
+this.powerSave = true
 }
 
 Thermostat.prototype.raiseTemperature = function() {
-  this.temperature += 1;
+  if (this.temperature < this.maximumTemperature) {
+    this.temperature += 1;
+  } else {
+    throw TypeError("Maximum temperature reached");
+  }
 };
 
 Thermostat.prototype.lowerTemperature = function() {
   if (this.temperature > MINIMUM_TEMPERATURE) {
-  this.temperature -= 1;
+    this.temperature -= 1;
   } else {
-  throw TypeError("Minimum temperature reached");
+    throw TypeError("Minimum temperature reached");
   }
+};
+
+Thermostat.prototype.changePowerSaveMode = function() {
+  this.powerSave = !this.powerSave;
 };
