6db4ef27a00d4a80ab63fb1271e035335043eeab
pflarcology <rhrg500@gmail.com>
2016-12-08 15:45:35 +0000

    add city options

--- src/interface.js
+++ src/interface.js
@@ -11,30 +11,37 @@ $( document ).ready(function() {
   currenttemp.text(thermostat.showTemperature());
   energyusage.text(thermostat.energyUsage());
   powersavestatus.text(thermostat.showPowerSaveMode());
+  $.get('http://api.openweathermap.org/data/2.5/weather?id=2643743&appid=669388914f66619c340185cc202f2758&units=metric', function(data) {
+    $('#weather').text(data.main.temp);
+  });
 });
 
 
 raisetemp.click(function (event) {
-thermostat.raiseTemperature();
-currenttemp.text(thermostat.showTemperature());
-energyusage.text(thermostat.energyUsage());
+  thermostat.raiseTemperature();
+  currenttemp.text(thermostat.showTemperature());
+  energyusage.text(thermostat.energyUsage());
 });
 
-lowertemp.click(function (event) {
-thermostat.lowerTemperature();
-currenttemp.text(thermostat.showTemperature());
-energyusage.text(thermostat.energyUsage());
+$('#currentcity').change(function (event) {
 
+})
+
+
+lowertemp.click(function (event) {
+  thermostat.lowerTemperature();
+  currenttemp.text(thermostat.showTemperature());
+  energyusage.text(thermostat.energyUsage());
 });
 
 reset.click(function (event) {
-thermostat.resetTemperature();
-currenttemp.text(thermostat.showTemperature());
-energyusage.text(thermostat.energyUsage());
+  thermostat.resetTemperature();
+  currenttemp.text(thermostat.showTemperature());
+  energyusage.text(thermostat.energyUsage());
 });
 
 powersavetoggle.click(function (event) {
-thermostat.changePowerSaveMode();
-powersavestatus.text(thermostat.showPowerSaveMode());
-energyusage.text(thermostat.energyUsage());
+  thermostat.changePowerSaveMode();
+  powersavestatus.text(thermostat.showPowerSaveMode());
+  energyusage.text(thermostat.energyUsage());
 });
