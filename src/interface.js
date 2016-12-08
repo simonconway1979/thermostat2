f658ebdfcc46916678dfe1051656aaf4fd0a0b49
pflarcology <rhrg500@gmail.com>
2016-12-08 14:29:39 +0000

    finishes interface

--- src/interface.js
+++ src/interface.js
@@ -5,25 +5,36 @@ var reset = $('#reset');
 var powersavestatus = $('#powersavestatus');
 var powersavetoggle = $('#powersavetoggle');
 var currenttemp = $('#currenttemp');
-var eneryusage = $('#energyusage');
+var energyusage = $('#energyusage');
+
+$( document ).ready(function() {
+  currenttemp.text(thermostat.showTemperature());
+  energyusage.text(thermostat.energyUsage());
+  powersavestatus.text(thermostat.showPowerSaveMode());
+});
 
 
 raisetemp.click(function (event) {
 thermostat.raiseTemperature();
 currenttemp.text(thermostat.showTemperature());
+energyusage.text(thermostat.energyUsage());
 });
 
 lowertemp.click(function (event) {
 thermostat.lowerTemperature();
 currenttemp.text(thermostat.showTemperature());
+energyusage.text(thermostat.energyUsage());
+
 });
 
 reset.click(function (event) {
 thermostat.resetTemperature();
 currenttemp.text(thermostat.showTemperature());
+energyusage.text(thermostat.energyUsage());
 });
 
 powersavetoggle.click(function (event) {
 thermostat.changePowerSaveMode();
 powersavestatus.text(thermostat.showPowerSaveMode());
+energyusage.text(thermostat.energyUsage());
 });
